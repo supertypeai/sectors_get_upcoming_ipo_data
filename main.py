6282b66d6ef55cb416dcc9712a9f9aea388559aa
unknown <projectwilsen@gmail.com>
2024-02-05 21:14:24 +0700

    add

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import json
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 chrome_options = Options()
 options = [
-    "--headless",
+    # "--headless",
     f"--user-agent={user_agent}"
 ]
 for option in options:
