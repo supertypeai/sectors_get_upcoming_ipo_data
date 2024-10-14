5ee9ae101b353721c4b1c4db662c45da6f7656fd
aidityas <aidityasadhakim250@gmail.com>
2024-10-14 14:35:29 +0700

    fix(scrapper): Fix the bug in scrapper also add logging and refactor code format

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import pandas as pd
+import numpy as np
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import json
@@ -9,14 +10,17 @@ ssl._create_default_https_context = ssl._create_unverified_context
 import urllib.request
 import translators as ts
 import os
+import logging
+from imp import reload
 from dotenv import load_dotenv
 load_dotenv()
 
-proxy = os.environ.get("proxy")
+def initiate_logging(LOG_FILENAME):
+    reload(logging)
 
-proxy_support = urllib.request.ProxyHandler({'http': proxy,'https': proxy})
-opener = urllib.request.build_opener(proxy_support)
-urllib.request.install_opener(opener)
+    formatLOG = '%(asctime)s - %(levelname)s: %(message)s'
+    logging.basicConfig(filename=LOG_FILENAME,level=logging.INFO, format=formatLOG)
+    logging.info('Program started')
 
 def convert_date(date_str):
     date_object = datetime.strptime(date_str, "%d %b %Y")
@@ -58,87 +62,117 @@ def translate_to_english(text):
     translated_text = ts.translate_text(text, translator="google", from_language='id',to_language='en')
     return translated_text
 
-result = {
-    "ticker_code" : [],
-    "company" : [],
-    "book_building_period" : [],
-    "book_building_price_range" : [],
-    "sector" : [],
-    "sub_sector":[],
-    "line_of_business_id":[],
-    "company_overview_id":[],
-    "address": [],
-    "website": [],
-    "number_of_shares_offered" : [],
-    "percent_of_total_shares": [],
-    "participant_admin": [],
-    "underwriter": [],
-    "updated_on": [],
-}
+if __name__ == '__main__':
+    LOG_FILENAME = 'scraper.log'
+    initiate_logging(LOG_FILENAME)
 
-url = f'https://e-ipo.co.id/en/ipo/index?per-page=&query=&sort=-updated_at&status_id=2&view=list'
-with urllib.request.urlopen(url) as response:
-    html = response.read()
-    print(html)
-soup = BeautifulSoup(html, 'html.parser')
+    try:
+        try:
+            proxy = os.environ.get("proxy")
 
-names = []
-names_class = soup.find_all(class_="margin-left10 colorwhite")
-for name in names_class:
-    company_name, symbol = name.get_text().replace("Sharia", "").replace(")", "").split(' (')
-    result["ticker_code"].append(symbol.replace("Closed", "").replace("Book Building",""))
-    result["company"].append(company_name)
+            proxy_support = urllib.request.ProxyHandler({'http': proxy,'https': proxy})
+            opener = urllib.request.build_opener(proxy_support)
+            urllib.request.install_opener(opener)
+        except Exception as e:
+            raise Exception("Error setting up proxy: {str(e)}")
+            
+        result = {
+            "ticker_code" : [],
+            "company" : [],
+            "book_building_period" : [],
+            "book_building_price_range" : [],
+            "sector" : [],
+            "sub_sector":[],
+            "line_of_business_id":[],
+            "company_overview_id":[],
+            "address": [],
+            "website": [],
+            "number_of_shares_offered" : [],
+            "percent_of_total_shares": [],
+            "participant_admin": [],
+            "underwriter": [],
+            "updated_on": [],
+        }
 
-notopmargins = soup.find_all("p", class_="notopmargin")
-nobottommargins = soup.find_all(class_="nobottommargin")
-for top, bottom in zip(notopmargins, nobottommargins):
-    if bottom.get_text() == "Sector": result["sector"].append(top.get_text())
-    elif bottom.get_text() == "Book Building Period": result["book_building_period"].append(top.get_text())
-    elif bottom.get_text() == "Book Building Price Range": result["book_building_price_range"].append(top.get_text())
-    elif bottom.get_text() == "Stock Offered": result["number_of_shares_offered"].append(top.get_text())
- 
-# click details
-buttons = soup.find_all(class_="button button-3d button-small notopmargin button-rounded button-dirtygreen")
-for button in buttons:
-    href = button.get("href")
-    new_url = f"https://e-ipo.co.id{href}"
-    company_info = extract_company_info(new_url)
-    print(company_info)
-    result["sub_sector"].append(company_info['Subsector'])
-    result["line_of_business_id"].append(company_info['Line Of Business'])
-    result["company_overview_id"].append(company_info['Company Overview'])
-    result["address"].append(company_info['Address'])
-    result["website"].append(company_info['Website'])
-    result["percent_of_total_shares"].append(float(company_info['% of Total Shares']))
-    result["participant_admin"].append(company_info['Participant Admin'])
-    result["underwriter"].append(company_info['Underwriter(s)'])
-    now = datetime.now()
-    result["updated_on"].append(now.strftime("%Y-%m-%d %H:%M:%S"))
+        try:
+            url = f'https://e-ipo.co.id/en/ipo/index?per-page=&query=&sort=-updated_at&status_id=2&view=list'
+            with urllib.request.urlopen(url) as response:
+                html = response.read()
+                print(html)
+            soup = BeautifulSoup(html, 'html.parser')
+        except Exception as e:
+            raise Exception(f"Error fetching data: {str(e)}")
 
-ipo = pd.DataFrame(result)
+        try:
+            names = []
+            names_class = soup.find_all(class_="margin-left10 colorwhite")
+            for name in names_class:
+                company_name, symbol = name.get_text().replace("Sharia", "").replace(")", "").split(' (')
+                result["ticker_code"].append(symbol.replace("Closed", "").replace("Book Building",""))
+                result["company"].append(company_name)
 
-if ipo.empty:
-    print("There's no upcoming ipo")
-else:
-    ipo['number_of_shares_offered'] = ipo['number_of_shares_offered'].str.replace(' Lot', '').str.replace(',', '', regex=True).astype(float)
-    ipo['book_building_price_range'] = ipo['book_building_price_range'].str.replace('IDR', '').str.replace("\xa0", "").str.replace(',', '', regex=True).astype(str)
-    ipo['company_overview'] = ipo['company_overview_id'].apply(translate_to_english)
-    ipo['line_of_business'] = ipo['line_of_business_id'].apply(translate_to_english)
-    ipo.drop(columns=['line_of_business_id','company_overview_id'], inplace=True)
-    ipo = ipo[['updated_on','ticker_code','company','book_building_period','book_building_price_range','sector','sub_sector','line_of_business','company_overview','address','website','number_of_shares_offered','percent_of_total_shares','participant_admin','underwriter']]
+            notopmargins = soup.find_all("p", class_="notopmargin")
+            nobottommargins = soup.find_all(class_="nobottommargin")
+            for top, bottom in zip(notopmargins, nobottommargins):
+                if bottom.get_text() == "Sector": result["sector"].append(top.get_text())
+                elif bottom.get_text() == "Book Building Period": result["book_building_period"].append(top.get_text())
+                elif bottom.get_text() == "Book Building Price Range": result["book_building_price_range"].append(top.get_text())
+                elif bottom.get_text() == "Stocks Offered": result["number_of_shares_offered"].append(top.get_text())
+        except Exception as e:
+            raise Exception(f"Error extracting data: {str(e)}")
+        
+        try:
+            # click details
+            buttons = soup.find_all(class_="button button-3d button-small notopmargin button-rounded button-dirtygreen")
+            for button in buttons:
+                href = button.get("href")
+                new_url = f"https://e-ipo.co.id{href}"
+                company_info = extract_company_info(new_url)
+                print(company_info)
+                result["sub_sector"].append(company_info['Subsector'])
+                result["line_of_business_id"].append(company_info['Line Of Business'])
+                result["company_overview_id"].append(company_info['Company Overview'])
+                result["address"].append(company_info['Address'])
+                result["website"].append(company_info['Website'])
+                result["percent_of_total_shares"].append(float(company_info['% of Total Shares']))
+                result["participant_admin"].append(company_info['Participant Admin'])
+                result["underwriter"].append(company_info['Underwriter(s)'])
+                now = datetime.now()
+                result["updated_on"].append(now.strftime("%Y-%m-%d %H:%M:%S"))
+            ipo = pd.DataFrame(result)
+        except Exception as e:
+            raise Exception(f"Error retrieve company: {str(e)}")
 
-    existing_ipo_data = pd.read_csv('https://raw.githubusercontent.com/supertypeai/sectors_get_upcoming_ipo_data/main/ipo.csv')
-    ipo_history_data = pd.concat([existing_ipo_data, ipo])
+        try:
+            if ipo.empty:
+                print("There's no upcoming ipo")
+            else:
+                ipo['number_of_shares_offered'] = ipo['number_of_shares_offered'].str.replace(' Lot', '').str.replace(',', '', regex=True).astype(float)
+                ipo['book_building_price_range'] = ipo['book_building_price_range'].str.replace('IDR', '').str.replace("\xa0", "").str.replace(',', '', regex=True).astype(str)
+                ipo['company_overview'] = ipo['company_overview_id'].apply(translate_to_english)
+                ipo['line_of_business'] = ipo['line_of_business_id'].apply(translate_to_english)
+                ipo.drop(columns=['line_of_business_id','company_overview_id'], inplace=True)
+                ipo = ipo[['updated_on','ticker_code','company','book_building_period','book_building_price_range','sector','sub_sector','line_of_business','company_overview','address','website','number_of_shares_offered','percent_of_total_shares','participant_admin','underwriter']]
 
-    ipo_history_data['updated_on'] = pd.to_datetime(ipo_history_data['updated_on'])
-    six_months_prior = now - relativedelta(months=6)
-    ipo_history_data = ipo_history_data[(ipo_history_data['updated_on'] >= six_months_prior) & (ipo_history_data['updated_on'] <= now)]
-    ipo_history_data = ipo_history_data.sort_values(by='updated_on').drop_duplicates(subset='ticker_code', keep='last')
-    ipo_history_data.to_csv("ipo.csv",index = False)
+                existing_ipo_data = pd.read_csv('https://raw.githubusercontent.com/supertypeai/sectors_get_upcoming_ipo_data/main/ipo.csv')
+                ipo_history_data = pd.concat([existing_ipo_data, ipo])
 
-    ipo.drop(columns=['updated_on'], inplace=True)
+                ipo_history_data['updated_on'] = pd.to_datetime(ipo_history_data['updated_on'])
+                six_months_prior = now - relativedelta(months=6)
+                ipo_history_data = ipo_history_data[(ipo_history_data['updated_on'] >= six_months_prior) & (ipo_history_data['updated_on'] <= now)]
+                ipo_history_data = ipo_history_data.sort_values(by='updated_on').drop_duplicates(subset='ticker_code', keep='last')
+                ipo_history_data.to_csv("ipo.csv",index = False)
 
-    upcoming_ipo_json = ipo.to_dict(orient='records')
+                ipo.drop(columns=['updated_on'], inplace=True)
 
-    with open('upcoming_ipo.json', 'w') as json_file:
-        json.dump(upcoming_ipo_json, json_file, indent=4)
+                upcoming_ipo_json = ipo.to_dict(orient='records')
+
+                with open('upcoming_ipo.json', 'w') as json_file:
+                    json.dump(upcoming_ipo_json, json_file, indent=4)
+        except Exception as e:
+            raise Exception(f"Error processing data: {str(e)}")
+        logging.info(f"Finish scrape {len(ipo['ticker_code'])} upcoming ipo data")
+            
+    except Exception as e:
+        logging.info(f'Program finished with error: {str(e)}')
+        print(f"Error: {str(e)}")
